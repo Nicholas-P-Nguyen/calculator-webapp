f78cc1ff8bfc5e8dfd96f64004a719c1aba5f96a
Nicholas Nguyen <nicholas.p.nguyen@gmail.com>
2024-01-09 15:46:32 -0500

    buttons now display on screen properly

--- calculator.js
+++ calculator.js
@@ -1,3 +1,6 @@
+// global variables to store the number
+let number;
+
 // Reference to nodes in the DOM 
 const btnZero = document.querySelector('.zero');
 const btnDot = document.querySelector('.dot');
@@ -15,34 +18,145 @@ const btnEight = document.querySelector('.eight');
 const btnNine = document.querySelector('.nine');
 const btnMultiply = document.querySelector('.multiply');
 const btnClear = document.querySelector('.clear');
-const btnSigned = document.querySelector('.signed');
+const btnDelete = document.querySelector('.delete');
 const btnRemainder = document.querySelector('.remainder');
 const btnDivide = document.querySelector('.divide');
 
-// Adding event listeners for all of the references to nodes
+const operation = document.querySelector('.operation');
+const result = document.querySelector('.result');
+
+// numbers
+btnZero.addEventListener('click', (event) => {
+    number = event.target.id;
+    displayContent(number);
+});
+
+btnOne.addEventListener('click', (event) => {
+    number = event.target.id;
+    displayContent(number);
+});
+btnTwo.addEventListener('click', (event) => {
+    number = event.target.id;
+    displayContent(number);
+});
+btnThree.addEventListener('click', (event) => {
+    number = event.target.id;
+    displayContent(number);
+});
+btnFour.addEventListener('click', (event) => {
+    number = event.target.id;
+    displayContent(number);
+});
+btnFive.addEventListener('click', (event) => {
+    number = event.target.id;
+    displayContent(number);
+});
+btnSix.addEventListener('click', (event) => {
+    number = event.target.id;
+    displayContent(number);
+});
+btnSeven.addEventListener('click', (event) => {
+    number = event.target.id;
+    displayContent(number);
+});
+btnEight.addEventListener('click', (event) => {
+    number = event.target.id;
+    displayContent(number);
+});
+btnNine.addEventListener('click', (event) => {
+    number = event.target.id;
+    displayContent(number);
+});
+// operations 
+btnDot.addEventListener('click', (event) => {
+    operator = event.target.id;
+    displayContent(operator);
+});
+btnPlus.addEventListener('click', (event) => {
+    operator = event.target.id;
+    displayContent(operator);
+});
+btnMinus.addEventListener('click', (event) => {
+    operator = event.target.id;
+    displayContent(operator);
+});
+btnMultiply.addEventListener('click', (event) => {
+    operator = event.target.id;
+    displayContent(operator);
+});
+btnDivide.addEventListener('click', (event) => {
+    operator = event.target.id;
+    displayContent(operator);
+});
+btnRemainder.addEventListener('click', (event) => {
+    operator = event.target.id;
+    displayContent(operator);
+});
+
 /*
-btnZero.addEventListener('click', );
-btnDot.addEventListener('click', )
-btnEqual.addEventListener('click', )
-btnOne.addEventListener('click', )
-btnTwo.addEventListener('click', )
-btnThree.addEventListener('click', )
-btnPlus.addEventListener('click', )
-btnFour.addEventListener('click', )
-btnFive.addEventListener('click', )
-btnSix.addEventListener('click', )
-btnMinus.addEventListener('click', )
-btnSeven.addEventListener('click', )
-btnEight.addEventListener('click', )
-btnNine.addEventListener('click', )
-btnMultiply.addEventListener('click', )
-btnClear.addEventListener('click', )
-btnSigned.addEventListener('click', )
-btnRemainder.addEventListener('click', )
-btnDivide.addEventListener('click', )
+btnEqual.addEventListener('click', (event) => {
+    operator = event.target.id;
+    displayContent(operator);
+});
+btnClear.addEventListener('click', (event) => {
+    operator = event.target.id;
+    displayContent(operator);
+});
+btnDelete.addEventListener('click', (event) => {
+    operator = event.target.id;
+    displayContent(operator);
+});
 */
+
+function displayContent(btnClicked)
+{
+    operation.innerHTML += btnClicked;
+}
 
 function add(x, y) 
 {
-    
+    return x + y;
+}
+
+function subtract(x, y)
+{
+    return x - y;
+}
+
+function multiply(x, y)
+{
+    return x * y;
+}
+
+function divide(x, y)
+{
+    return x / y;
+}
+
+function remainder(x, y) 
+{
+    return x % y;
+}
+
+function operate(x, y, operator) 
+{
+    switch(operator) 
+    {
+        case "+":
+            add(x, y);
+            break;
+        case "-":
+            subtract(x, y);
+            break;
+        case "x":
+            multiply(x, y);
+            break;
+        case "/":
+            divide(x, y);
+            break;
+        case "%":
+            remainder(x, y);
+            break;
+    }
+        
 }
