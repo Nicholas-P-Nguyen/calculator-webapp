66ec2cb13e649623ba871d5a2ec6f9df4d23c517
Nicholas Nguyen <nicholas.p.nguyen@gmail.com>
2024-01-09 15:00:32 -0500

    Got the basic structure of the layout completed with HTML and CSS

---
+++ calculator.js
@@ -0,0 +1,48 @@
+// Reference to nodes in the DOM 
+const btnZero = document.querySelector('.zero');
+const btnDot = document.querySelector('.dot');
+const btnEqual = document.querySelector('.equal');
+const btnOne = document.querySelector('.one');
+const btnTwo = document.querySelector('.two');
+const btnThree = document.querySelector('.three');
+const btnPlus = document.querySelector('.plus');
+const btnFour = document.querySelector('.four');
+const btnFive = document.querySelector('.five');
+const btnSix = document.querySelector('.six');
+const btnMinus = document.querySelector('.minus');
+const btnSeven = document.querySelector('.seven');
+const btnEight = document.querySelector('.eight');
+const btnNine = document.querySelector('.nine');
+const btnMultiply = document.querySelector('.multiply');
+const btnClear = document.querySelector('.clear');
+const btnSigned = document.querySelector('.signed');
+const btnRemainder = document.querySelector('.remainder');
+const btnDivide = document.querySelector('.divide');
+
+// Adding event listeners for all of the references to nodes
+/*
+btnZero.addEventListener('click', );
+btnDot.addEventListener('click', )
+btnEqual.addEventListener('click', )
+btnOne.addEventListener('click', )
+btnTwo.addEventListener('click', )
+btnThree.addEventListener('click', )
+btnPlus.addEventListener('click', )
+btnFour.addEventListener('click', )
+btnFive.addEventListener('click', )
+btnSix.addEventListener('click', )
+btnMinus.addEventListener('click', )
+btnSeven.addEventListener('click', )
+btnEight.addEventListener('click', )
+btnNine.addEventListener('click', )
+btnMultiply.addEventListener('click', )
+btnClear.addEventListener('click', )
+btnSigned.addEventListener('click', )
+btnRemainder.addEventListener('click', )
+btnDivide.addEventListener('click', )
+*/
+
+function add(x, y) 
+{
+    
+}
